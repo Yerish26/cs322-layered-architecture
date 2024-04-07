a25ac8fa03f8d7231c7663b5147b3056b018e5d0
Yerish26 <yeghiaa@gmail.com>
2024-04-07 23:24:48 +0400

    Moved presentation into the application

--- webapp/src/main/java/am/cs322/Presentation/UserController.java
+++ webapp/src/main/java/am/cs322/Presentation/UserController.java
@@ -1,6 +1,7 @@
-package am.cs322;
+package am.cs322.Presentation;
 
-import am.cs322.model.CreateUserRequest;
+
+import am.cs322.Presentation.model.CreateUserRequest;
 import am.cs322.model.UserDTO;
 
 public interface UserController {
